4ffc46a30db4ff3f49e971818c0e022c717e840b
Fausnightm <m.fausnight@lafilm.edu>
2021-07-03 14:42:27 -0400

    added query selectors for some of the html elements

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,32 @@
-fetch('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}')
+var apiKey = "&appid=686bd4df5b28f74a8ccbdd4feea3d8e3"
+
+var cityEl = document.querySelector("#city")
+
+var cityContainer = document.querySelector("#weather-form")
+
+var localStorage = document.querySelector("#local-storage")
+
+var currentWeather = document.querySelector("#current-weather")
+
+var forecast = document.querySelector("#forecast")
+
+
+var submitCity = function(event) {
+    event.preventDefault ();
+
+    var searchBar = cityEl.value.trim ();
+
+    if (searchBar) {
+        
+    }
+
+}
+
+fetch('http://api.openweathermap.org/data/2.5/forecast?q=cleveland&appid=686bd4df5b28f74a8ccbdd4feea3d8e3')
 .then(response => {
     return response.json();
 })
 .then(weather => {
-    console.log(weather);
+    console.log(weather.temp);
 
 });
